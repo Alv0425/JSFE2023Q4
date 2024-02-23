0eea8e4d774e6c11b13fb76696b8da0f4adca6a7
Alv0425 <vilejshikova@gmail.com>
2024-02-23 16:31:34 +0300

    feat: update sources.ts with applying types and interfaces

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -1,27 +1,28 @@
 import './sources.css';
-import { ISource } from '../../auxiliary/interfaces';
-import { isHTMLElement } from '../../auxiliary/checks';
+import { Source } from '../../auxiliary/interfaces';
+import { getElementOfType } from '../../auxiliary/checks';
 
-interface ISources {
-    draw: (data: ISource[]) => void;
+interface SourcesInterface {
+    draw: (data: Source[]) => void;
 }
 
-class Sources implements ISources {
-    draw(data : ISource[]) {
+class Sources implements SourcesInterface {
+    draw(data: Source[]) {
         const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
-        if (!(sourceItemTemp instanceof HTMLTemplateElement)) throw new Error(`sourceItemTemp is not HTMLTemplateElement`);
+        const sourceItemTemp = getElementOfType(HTMLTemplateElement, document.querySelector('#sourceItemTemp'));
+
         data.forEach((item) => {
             const sourceClone = sourceItemTemp.content.cloneNode(true);
-            if (!(sourceClone instanceof DocumentFragment)) throw new Error('sourceClone in not instance of DocumentFragment');
-            const sourceItemName = isHTMLElement(sourceClone.querySelector('.source__item-name'));
-            const sourceItem = isHTMLElement(sourceClone.querySelector('.source__item'));
+            if (!(sourceClone instanceof DocumentFragment))
+                throw new Error('sourceClone in not instance of DocumentFragment');
+            const sourceItemName = getElementOfType(HTMLElement, sourceClone.querySelector('.source__item-name'));
+            const sourceItem = getElementOfType(HTMLElement, sourceClone.querySelector('.source__item'));
             sourceItemName.textContent = item.name;
             sourceItem.setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
-        const sourcesElement  = isHTMLElement(document.querySelector('.sources'))
+        const sourcesElement = getElementOfType(HTMLElement, document.querySelector('.sources'));
         sourcesElement.append(fragment);
     }
 }
