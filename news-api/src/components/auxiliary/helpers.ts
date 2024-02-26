0bc7d027730a6b56c4b95e52bd01d1f47f126b94
Alv0425 <vilejshikova@gmail.com>
2024-02-26 13:18:41 +0300

    feat: update and rename (checks to helpers) module with helper generics and functions

---
+++ news-api/src/components/auxiliary/helpers.ts
@@ -0,0 +1,41 @@
+import { TypeOfAncestor, ResponseSources, ResponseNews } from './interfaces';
+
+export function assertsElementIs<T>(ancestor: TypeOfAncestor<T>, element: unknown): asserts element is T {
+    if (element === null) throw new Error(`element is null`);
+    if (!(element instanceof ancestor)) throw new Error(`${element} is not instance of ${ancestor}`);
+}
+
+export function getElementOfType<T extends Node | Element | Document | EventTarget>(
+    ancestor: TypeOfAncestor<T>,
+    element: Node | Element | EventTarget | null
+): T {
+    assertsElementIs<T>(ancestor, element);
+    return element;
+}
+
+type KeyValuePairs = Record<string, string>;
+
+export function isResponseNews(res: ResponseNews | ResponseSources): res is ResponseNews {
+    return typeof res === 'object' && res !== undefined && res !== null && 'status' in res && 'articles' in res;
+}
+
+export function isResponseSources(res: ResponseNews | ResponseSources): res is ResponseSources {
+    return typeof res === 'object' && res !== null && res !== undefined && 'status' in res && 'sources' in res;
+}
+
+export function createNode(type: string, classname?: string[], attributes?: KeyValuePairs) {
+    const newElement = document.createElement(type);
+    if (!classname) return newElement;
+    newElement.classList.add(...classname);
+    if (!attributes) return newElement;
+    for (const key in attributes) {
+        newElement.setAttribute(key, attributes[key]);
+    }
+    return newElement;
+}
+
+export function clearNode(node: HTMLElement) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
