b605c25dd624f508578ae8fd0de0003c4bf2395a
Alv0425 <vilejshikova@gmail.com>
2024-02-26 14:26:59 +0300

    feat: add to app view methods for rendering of search bar

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,19 +1,23 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import Layout from './layout/layout';
 import { ResponseSources, ResponseNews } from '../auxiliary/interfaces';
 
 interface AppViewInterface {
     drawNews: (data: ResponseNews) => void;
     drawSources: (data: ResponseSources) => void;
+    drawSearchField: () => void;
 }
 
 export class AppView implements AppViewInterface {
     private news: News;
     private sources: Sources;
+    private layout: Layout;
 
     constructor() {
         this.news = new News();
         this.sources = new Sources();
+        this.layout = new Layout();
     }
 
     public drawNews(data: ResponseNews): void {
@@ -24,6 +28,11 @@ export class AppView implements AppViewInterface {
     public drawSources(data: ResponseSources): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
+    }
+
+    public drawSearchField(): Record<string, HTMLElement> {
+        const obj = this.layout.drawSearchBar();
+        return obj;
     }
 }
 
