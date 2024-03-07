c7b5b048f66c2cf1276833668ed9c4567cfa305d
Elena Vilejshikova <72615388+Alv0425@users.noreply.github.com>
2024-03-08 00:41:50 +0300

    chore: configure webpack, husky, eslint and ts (#75)
    * chore: install dev dependencies and configure husky
    
    * chore: configure typescript and prettier
    
    * chore: configure eslint and webpack to work with typescript
    
    * chore: update husky scripts
    
    * chore: update configuration for eslint and update scripts for webpack
    
    * chore: update husky pre-push script

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const EslintPlug = require("eslint-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const base = {
+  entry: path.resolve(__dirname, "./src/index"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+      {
+        test: /\.(png|jpg|svg|ico)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "./dist"),
+    assetModuleFilename: (assetsPath) => {
+      const assetDest = path
+        .dirname(assetsPath.filename)
+        .split("/")
+        .slice(1)
+        .join("/");
+      return `${assetDest}/[name].[ext]`;
+    },
+  },
+  plugins: [
+    new EslintPlug({ extensions: "ts" }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/images",
+          to: "assets",
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  if (mode === "prod") return merge(base, require("./webpack.prod.config"));
+  return merge(base, require("./webpack.dev.config"));
+};
