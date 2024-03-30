f3fa59b71bd09a488b6f28ef468de41e20ff60f8
Alv0425 <vilejshikova@gmail.com>
2024-03-30 21:45:55 +0300

    feat: implement methods for the edditing cars collection

--- async-race/src/ui/garage/garage-controls/generate-cars.ts
+++ async-race/src/ui/garage/garage-controls/generate-cars.ts
@@ -1,9 +1,11 @@
+import eventEmitter from "../../../services/event-emitter";
 import Component from "../../../utils/component";
 
 class GenerateCarsControl extends Component {
   constructor() {
     super("button", ["garage__control-generate"], {}, { type: "button" });
     this.setTextContent("GENERATE 100 CARS");
+    this.addListener("click", () => eventEmitter.emit("generate-random-cars"));
   }
 }
 
--- async-race/src/ui/garage/garage-controls/update-control.ts
+++ async-race/src/ui/garage/garage-controls/update-control.ts
@@ -1,3 +1,5 @@
+import { getCarByID } from "../../../services/api/get-cars";
+import eventEmitter from "../../../services/event-emitter";
 import Component from "../../../utils/component";
 import { button, input } from "../../../utils/elements";
 
@@ -14,6 +16,36 @@ class ControlUpdate extends Component {
     this.color = input(["garage__control-color"], { type: "color", value: "#000" });
     this.button = button(["garage__control-button"], "UPDATE");
     this.appendContent([this.input, this.color, this.button]);
+    this.button.addListener("click", async () => {
+      if (this.getId() === "") return;
+      const car = await getCarByID(parseInt(this.getId(), 10));
+      if (car.name) eventEmitter.emit("edit-car");
+      if (!car.name) console.log("Select another car to edit!");
+    });
+  }
+
+  getId() {
+    return this.button.getComponent().value;
+  }
+
+  getColor() {
+    return this.color.getComponent().value;
+  }
+
+  getName() {
+    return this.input.getComponent().value;
+  }
+
+  setProps(id: number, name: string, color: string) {
+    this.button.getComponent().value = `${id}`;
+    this.input.getComponent().value = name;
+    this.color.getComponent().value = color;
+  }
+
+  resetInputs() {
+    this.button.getComponent().value = "";
+    this.input.getComponent().value = "";
+    this.color.getComponent().value = "#000000";
   }
 }
 
