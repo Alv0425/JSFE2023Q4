6890365bd3ac32d20c511d47036f8fd50dabb961
Alv0425 <vilejshikova@gmail.com>
2024-04-08 03:13:33 +0300

    fix: fix generate-random-color function

--- async-race/src/utils/generate-random-color.ts
+++ async-race/src/utils/generate-random-color.ts
@@ -1,5 +1,7 @@
-function generateRandoColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function generateRandomColor() {
+  const hexAlphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+  const colorArray = new Array(6).fill("").map(() => hexAlphabet[Math.round(Math.random() * (hexAlphabet.length - 1))]);
+  return `#${colorArray.join("")}`;
 }
 
-export default generateRandoColor;
+export default generateRandomColor;
