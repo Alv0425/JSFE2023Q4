94e14de1a1aa590f503bdeb4aa9834cb41618314
Elena Vilejshikova <72615388+Alv0425@users.noreply.github.com>
2024-04-15 02:11:59 +0300

    chore: FUN-CHAT-01 Prepare project directory and environment (#132)
    * chore: instll dependencies and configure husky
    
    * chore: configure eslint to work with typescript
    
    * chore: add typescript configuration file
    
    * chore: configure prettier
    
    * chore: configure webpack and create src directory
    
    * chore: update pre-commit script

---
+++ fun-chat/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const EslintPlugin = require("eslint-webpack-plugin");
+// const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.ts"),
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: (assetsPath) => {
+      const assetDest = path.dirname(assetsPath.filename).split("/").slice(1).join("/");
+      return `${assetDest}/[name].[ext]`;
+    },
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+      {
+        test: /\.(png|jpg|svg|ico)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new EslintPlugin({ extensions: "ts" }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+  ],
+};
