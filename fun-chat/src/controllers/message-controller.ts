c59d6a65fb054fe29caf773b3018fc1e80795479
Alv0425 <vilejshikova@gmail.com>
2024-04-21 22:23:03 +0300

    feat: update chat and message controllers

--- fun-chat/src/controllers/message-controller.ts
+++ fun-chat/src/controllers/message-controller.ts
@@ -1,4 +1,4 @@
-import type { IRequestHistory, IRequestSend } from "../models/request";
+import type { IRequest, IRequestHistory, IRequestSend } from "../models/request";
 import AuthController from "./auth-controller";
 
 class MessageController {
@@ -25,6 +25,19 @@ class MessageController {
         message: {
           to: login,
           text,
+        },
+      },
+    };
+    return req;
+  }
+
+  public static setReadMessageStatus(id: string): IRequest {
+    const req = {
+      id: `MSG_READ_${id}`,
+      type: "MSG_READ",
+      payload: {
+        message: {
+          id,
         },
       },
     };
