dafb3c2cd0219c306782452996c378a34260af9e
Alv0425 <vilejshikova@gmail.com>
2024-04-21 13:41:10 +0300

    feat: implement websocket events map

---
+++ fun-chat/src/services/response-map.ts
@@ -0,0 +1,18 @@
+import MessageType from "../models/message-types";
+import { EventsMap } from "../utils/event-emitter/events";
+
+const responseMap: Map<MessageType, EventsMap> = new Map([
+  [MessageType.login, EventsMap.login],
+  [MessageType.logout, EventsMap.logout],
+  [MessageType.loginExternal, EventsMap.externalLogin],
+  [MessageType.logoutExternal, EventsMap.externalLogout],
+  [MessageType.error, EventsMap.error],
+  [MessageType.userActive, EventsMap.getActiveUsers],
+  [MessageType.userInactive, EventsMap.getInactiveUsers],
+  [MessageType.messageFrom, EventsMap.getHistory],
+  [MessageType.messageSend, EventsMap.messageSent],
+  // [MessageType.messageDelivered, ""],
+  // [MessageType.messageRead, ""],
+]);
+
+export default responseMap;
