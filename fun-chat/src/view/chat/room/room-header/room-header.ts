0a3b8a5e03f043bd26b265a25ec4aa35e6eecad6
Alv0425 <vilejshikova@gmail.com>
2024-04-23 02:52:33 +0300

    feat: update styles for small screens

--- fun-chat/src/view/chat/room/room-header/room-header.ts
+++ fun-chat/src/view/chat/room/room-header/room-header.ts
@@ -1,14 +1,18 @@
+import { EventsMap } from "../../../../utils/event-emitter/events";
 import generateColor from "../../../../utils/color-generator";
 import Component from "../../../../utils/component/component";
-import { div, h2 } from "../../../../utils/component/elements";
+import { button, div, h2, svgSprite } from "../../../../utils/component/elements";
+import eventEmitter from "../../../../utils/event-emitter/event-emitter";
 
 class RoomHeader extends Component {
   constructor(login: string) {
     super("div", ["room__header"], {}, {});
+    const back = button(["room__back"], "", svgSprite("./assets/icons/arrow-left-solid.svg#back", "room__button-icon"));
+    back.addListener("click", () => eventEmitter.emit(EventsMap.backRoomClicked));
     const circle = div(["room__circle"]);
     circle.setTextContent(login[0]?.toUpperCase() || "");
     circle.setStyleAttribute("background-color", generateColor(login));
-    this.appendContent([circle, h2(["room__login"], login)]);
+    this.appendContent([back, circle, h2(["room__login"], login)]);
   }
 }
 
