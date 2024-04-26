1cf3e0bf1c158089ce4332cd01bac47fb65f0d6c
Alv0425 <vilejshikova@gmail.com>
2024-04-27 00:46:30 +0300

    refactor: refactor User model

--- fun-chat/src/models/user.ts
+++ fun-chat/src/models/user.ts
@@ -1,42 +1,22 @@
-import eventEmitter from "../utils/event-emitter/event-emitter";
 import ContactView from "../view/chat/contact/contact-view";
-import type { Message } from "./message";
 import type { IUserResponse } from "./response";
-import { EventsMap } from "../utils/event-emitter/events";
 
-export interface IUser {
-  id: string;
-  login: string;
-  isLoginned: false;
-  password?: string;
-}
-
-export class User {
+class User {
   public status: boolean | undefined;
 
   private login: string;
 
-  private password: string | undefined;
-
   private view: ContactView;
-
-  private messages: Message[] = [];
 
   private numOfUnreadMessages = 0;
 
-  private messageIds: string[] = [];
-
-  private unreadMsgs: Set<string> = new Set();
-
   private textLastMessage = "";
 
-  constructor(params: IUserResponse, password?: string) {
+  constructor(params: IUserResponse) {
     this.status = params.isLogined;
     this.login = params.login;
-    this.password = password;
     this.view = new ContactView(this.login, this.numOfUnreadMessages, "", this.status || false);
     this.setNumOfUnread(0);
-    this.view.addListener("click", () => eventEmitter.emit(EventsMap.contactClicked, this.login));
   }
 
   public remove(): void {
@@ -58,32 +38,12 @@ export class User {
     this.view.setMessage(this.textLastMessage);
   }
 
-  public setStatus(status: boolean): void {
-    this.status = status;
-  }
-
   public getView(): ContactView {
     return this.view;
   }
 
   public getStatus(): boolean {
     return this.status ?? false;
-  }
-
-  public setHistoryIds(messageIds: string[]): void {
-    this.messageIds = messageIds;
-  }
-
-  public getHistoryIds(): string[] {
-    return this.messageIds;
-  }
-
-  public setHistory(messages: Message[]): void {
-    this.messages = messages;
-  }
-
-  public getHistory(): Message[] {
-    return this.messages;
   }
 
   public setNumOfUnread(n: number): void {
@@ -111,3 +71,5 @@ export class User {
     this.view.hide();
   }
 }
+
+export default User;
